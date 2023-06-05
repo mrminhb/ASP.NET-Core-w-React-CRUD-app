008856f76fa74fcd86b9de616957c06f1f7d2d3b
mrminhb <novafalcongamers@gmail.com>
2023-06-05 17:16:17 +0700

    add employee and employee context

---
+++ CRUD-BE/CRUD-BE/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace CRUD_BE.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ CRUD-BE/CRUD-BE/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_BE.Models;
+
+public class EmployeeContext : DbContext
+{
+    public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Employee> Employees { get; set; }
+}
